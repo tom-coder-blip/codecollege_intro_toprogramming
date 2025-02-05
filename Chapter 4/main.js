da07d2876020220748ffa591c51038f8f5f8104d
tom-coder-blip <vumatom10@gmail.com>
2025-02-05 09:49:46 +0200

    Publish Chapter 4

---
+++ Chapter 4/main.js
@@ -0,0 +1,96 @@
+var myPerson = {
+    "name": "Tom",
+    "age": "21",
+    "favouritecolor": "Beige"
+   };
+
+   var myObject = {
+
+   }
+
+   myObject["myFirstKey"] = "random string";
+   myObject["mySecondKey"] = false;
+
+   var favouriteFood = [
+       {
+
+            "food" : "Burger",
+            "restaurant" : "BurgerKing",
+            "numberofBurgersEaten" : 20000
+
+       },
+       {
+
+            "food" : "Lazania",
+            "restuarant" : "TopGBakery",
+            "numberofLazaniasEaten" : 1500
+
+       },
+       {
+
+             "food" : "BananaBread",
+             "restuarant" : "TomsBakery",
+             "numberofloavesEaten" : 4000
+
+       }
+           
+   ]
+
+   var countries = {
+    "Greece" : {
+        Continent : "Europe",
+        capitalCity : "Athens", 
+        funactivity : "ScubaDiving",
+        numberofpeople : 96000000
+    },
+
+    "Brazil": {
+        Continent : "SouthAmerica",
+        capitalCity : "RioDeJeneiro",
+        funActivity : "StreetFootball",
+        numberofPople : 77000000
+    },
+
+    "Ghana": {
+        Continent : "Africa",
+        capitalCity : "Accra",
+        funActivity : "NationalParks",
+        numberofPople : 79000000
+    }
+   }
+
+   var dinosaurs = ["T-Rex", "Velociraptor", "Stegosaurus",
+    "Triceratops", "Brachiosaurus", "Pteranodon", "Apatosaurus", 
+    "Diplodocus", "Compsognathus"];
+
+    var dinosaursAndNumbers = [3, "dinosaurs", ["triceratops",
+        "stegosaurus", 3627.5], 10];
+
+var animals = [];
+animals.push("Cat");
+1
+animals.push("Dog");
+2
+animals.push("Llama");
+3
+animals;
+["Cat", "Dog", "Llama"]
+animals.length;
+
+var colors = ["red", "green", "blue"];
+colors.indexOf("blue");
+2
+colors.indexOf("green");
+1
+
+var cat = { legs: 3, name: "Harmony", color: "Tortoiseshell" };
+
+var dinosaurs = [
+    { name: "Tyrannosaurus Rex", period: "Late Cretaceous" },
+    { name: "Stegosaurus", period: "Late Jurassic" },
+    { name: "Plateosaurus", period: "Triassic" }
+   ];
+
+
+   
+    
