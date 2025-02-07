f46d89e21b0c416311d78ac9977880e807b26fbe
tom-coder-blip <vumatom10@gmail.com>
2025-02-07 04:24:11 +0200

    Chapter7and8Code

---
+++ Chapter 8/main.js
@@ -0,0 +1,15 @@
+var eatFavouriteFood = function (myFavouriteFood, myFavouritDrink){
+    console.log("Lets eat " + myFavouriteFood);
+    console.log("Lets eat " + myFavouritDrink);
+};
+
+eatFavouriteFood("Burger");
+eatFavouriteFood("Lazania");
+eatFavouriteFood("Banana Bread");
+
+
+// var printMultipleTimes = function (howManyTimes, whatToDraw) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//     console.log(i + " " + whatToDraw);
+//     }
+//    };
---
+++ Chapter7/test.js
@@ -0,0 +1,21 @@
+//var colour = prompt("Whats is your favourite colour?");
+var yesgym = confirm("Is it true that you gym everday?" +
+     " Press Ok for True or Cancel for False");
+
+// alert("Boom");
+
+if (yesgym === true) {
+    alert("Buff guy!")
+
+} else{
+    alert("Come on bruuu.")
+};
+
+// //var testVariable = prompt("Insert something");
+
+// if (testVariable === 1){
+//     alert("That is a number.");
+// }else{
+//     alert("That is not a number")
+
+// };
