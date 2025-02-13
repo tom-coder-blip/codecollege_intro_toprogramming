0d29b88d79b332f569c7518f36df9368916a4713
tom-coder-blip <vumatom10@gmail.com>
2025-02-13 08:23:56 +0200

    DummiesChapter13,14

---
+++ test/test.js
@@ -0,0 +1,59 @@
+// for (let i = 0; i < 5; i++){
+//     alert(i);
+// }
+
+
+// var i = 0;
+
+// while (i < 5) {
+//     alert(i);
+//     i++;
+
+// };
+
+// function login(un, pw) {
+//     while (un === "TOM" && pw === "PASSWORD") {
+//         alert("Welcome");
+//         break;
+        
+//     };
+    
+// };
+
+// login(prompt("Username please?"), prompt("Password please?"));
+
+
+// function login(un, pw) {
+//     while (un === "DAVID" && pw === "PASSWORD") {
+//         welcome();
+//     };
+// };
+
+// function welcome() {
+//     alert("Welcome");
+// }
+
+// login(prompt("Username please?"), prompt("Password please?"));
+
+
+// for (let i = 1; i <=50; i++){
+//     if (i % 3 === 0 && i % 5 === 0){
+//         console.log("FizzBuzz");
+//     }else if (i % 3 === 0){
+//         console.log("Fizz");
+//     }else if (i % 5 === 0){
+//         console.log("Buzz");
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+
+let word = "JavaScript";
+let reversed = "";
+
+for (let i = word.length - 1; i >= 0; i--){
+    reversed += word[i];
+}
+
+console.log("Reversed Word:", reversed);
